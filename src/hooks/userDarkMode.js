4ec20bb763392caf72d84894f6b63592a42ea65f
Ayomide Jones <ayormeday@gmail.com>
2019-10-02 01:04:43 +0100

    userDarkMode completed

--- src/hooks/userDarkMode.js
+++ src/hooks/userDarkMode.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import { useEffect } from 'react';
 import { useLocalStorage } from './useLocalStorage';
 
 export const useDarkMode = (key, initialValue) => {
     const [value, setValue] = useLocalStorage(key, initialValue);
-    
 
-    return (
+    useEffect(() => {
+      return value ?  document.body.className ='dark-mode' : initialValue;  
+      }, initialValue);
 
-    );
+    return [value, setValue];
 }
